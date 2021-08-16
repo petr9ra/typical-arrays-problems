1b78190cde3244ce2c2b5d62d967328029e1d076
Petr <petr9ra@yandex.ru>
2021-08-16 19:44:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) 
+        return 0 
+    return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) 
+        return 0 
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) 
+        return 0;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i] 
+    };
+	let answer = +((sum / array.length).toFixed(2))
+    return answer
 }
